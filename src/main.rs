9309fb874c06da4e26b9a3ec9cb273325ca338b1
Matt Koski <maccam912@gmail.com>
2024-08-03 14:08:31 -0500

    ws2812 works

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,11 @@ use embassy_net::{
     tcp::client::{TcpClient, TcpClientState},
 };
 use embassy_rp::{
-    clocks::RoscRng,
-    rtc::{DayOfWeek, Rtc},
+    bind_interrupts, clocks::{self, RoscRng}, dma::{AnyChannel, Channel}, into_ref, peripherals::{DMA_CH1, PIO1}, pio::{Common, Config, FifoJoin, Instance, InterruptHandler, Pio, PioPin, ShiftConfig, ShiftDirection, StateMachine}, rtc::{DayOfWeek, Rtc}, Peripheral, PeripheralRef
 };
-use embassy_time::{Duration, Timer};
+use embassy_time::{Duration, Timer, Ticker};
+use fixed::types::U24F8;
+use fixed_macro::fixed;
 use log::{error, info};
 use rand::RngCore;
 use reqwless::{
@@ -23,6 +24,8 @@ use reqwless::{
     request::Method,
 };
 use serde::Deserialize;
+use smart_leds::RGB8;
+
 use {defmt_rtt as _, panic_probe as _};
 
 mod usblogger;
@@ -30,6 +33,129 @@ mod wifi;
 
 const WIFI_NETWORK: &str = "Bill Wi the Science Fi";
 const WIFI_PASSWORD: &str = "sciencerules";
+const NUM_LEDS: usize = 144;
+
+bind_interrupts!(struct Irqs {
+    PIO1_IRQ_0 => InterruptHandler<PIO1>;
+});
+
+pub struct Ws2812<'d, P: Instance, const S: usize, const N: usize> {
+    dma: PeripheralRef<'d, AnyChannel>,
+    sm: StateMachine<'d, P, S>,
+}
+
+impl<'d, P: Instance, const S: usize, const N: usize> Ws2812<'d, P, S, N> {
+    pub fn new(
+        pio: &mut Common<'d, P>,
+        mut sm: StateMachine<'d, P, S>,
+        dma: impl Peripheral<P = impl Channel> + 'd,
+        pin: impl PioPin,
+    ) -> Self {
+        into_ref!(dma);
+
+        // Setup sm0
+
+        // prepare the PIO program
+        let side_set = pio::SideSet::new(false, 1, false);
+        let mut a: pio::Assembler<32> = pio::Assembler::new_with_side_set(side_set);
+
+        const T1: u8 = 2; // start bit
+        const T2: u8 = 5; // data bit
+        const T3: u8 = 3; // stop bit
+        const CYCLES_PER_BIT: u32 = (T1 + T2 + T3) as u32;
+
+        let mut wrap_target = a.label();
+        let mut wrap_source = a.label();
+        let mut do_zero = a.label();
+        a.set_with_side_set(pio::SetDestination::PINDIRS, 1, 0);
+        a.bind(&mut wrap_target);
+        // Do stop bit
+        a.out_with_delay_and_side_set(pio::OutDestination::X, 1, T3 - 1, 0);
+        // Do start bit
+        a.jmp_with_delay_and_side_set(pio::JmpCondition::XIsZero, &mut do_zero, T1 - 1, 1);
+        // Do data bit = 1
+        a.jmp_with_delay_and_side_set(pio::JmpCondition::Always, &mut wrap_target, T2 - 1, 1);
+        a.bind(&mut do_zero);
+        // Do data bit = 0
+        a.nop_with_delay_and_side_set(T2 - 1, 0);
+        a.bind(&mut wrap_source);
+
+        let prg = a.assemble_with_wrap(wrap_source, wrap_target);
+        let mut cfg = Config::default();
+
+        // Pin config
+        let out_pin = pio.make_pio_pin(pin);
+        cfg.set_out_pins(&[&out_pin]);
+        cfg.set_set_pins(&[&out_pin]);
+
+        cfg.use_program(&pio.load_program(&prg), &[&out_pin]);
+
+        // Clock config, measured in kHz to avoid overflows
+        // TODO CLOCK_FREQ should come from embassy_rp
+        let clock_freq = U24F8::from_num(clocks::clk_sys_freq() / 1000);
+        let ws2812_freq = fixed!(800: U24F8);
+        let bit_freq = ws2812_freq * CYCLES_PER_BIT;
+        cfg.clock_divider = clock_freq / bit_freq;
+
+        // FIFO config
+        cfg.fifo_join = FifoJoin::TxOnly;
+        cfg.shift_out = ShiftConfig {
+            auto_fill: true,
+            threshold: 24,
+            direction: ShiftDirection::Left,
+        };
+
+        sm.set_config(&cfg);
+        sm.set_enable(true);
+
+        Self {
+            dma: dma.map_into(),
+            sm,
+        }
+    }
+
+    pub async fn write(&mut self, colors: &[RGB8; N]) {
+        // Precompute the word bytes from the colors
+        let mut words = [0u32; N];
+        for i in 0..N {
+            let word = (u32::from(colors[i].g) << 24) | (u32::from(colors[i].r) << 16) | (u32::from(colors[i].b) << 8);
+            words[i] = word;
+        }
+
+        // DMA transfer
+        self.sm.tx().dma_push(self.dma.reborrow(), &words).await;
+
+        Timer::after_micros(55).await;
+    }
+}
+
+/// Input a value 0 to 255 to get a color value
+/// The colours are a transition r - g - b - back to r.
+fn wheel(mut wheel_pos: u8) -> RGB8 {
+    wheel_pos = 255 - wheel_pos;
+    let scale = 10; // Adjust this value to control brightness (lower value = dimmer)
+    if wheel_pos < 85 {
+        return (
+            (255 - wheel_pos * 3) / scale,
+            0,
+            (wheel_pos * 3) / scale
+        ).into();
+    }
+    if wheel_pos < 170 {
+        wheel_pos -= 85;
+        return (
+            0,
+            (wheel_pos * 3) / scale,
+            (255 - wheel_pos * 3) / scale
+        ).into();
+    }
+    wheel_pos -= 170;
+    (
+        (wheel_pos * 3) / scale,
+        (255 - wheel_pos * 3) / scale,
+        0
+    ).into()
+}
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -43,6 +169,8 @@ async fn main(spawner: Spawner) {
     let mut rng = RoscRng;
     let seed = rng.next_u64();
     let mut rtc = Rtc::new(p.RTC);
+
+    spawner.spawn(ws2812_task(p.PIO1, p.DMA_CH1, p.PIN_4)).unwrap();
 
     loop {
         let mut rx_buffer = [0; 8192];
@@ -148,6 +276,23 @@ async fn main(spawner: Spawner) {
                 current_time.second
             );
             Timer::after(Duration::from_secs(1)).await;
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn ws2812_task(pio: PIO1, dma: DMA_CH1, pin: impl PioPin) {
+    let Pio { mut common, sm0, .. } = Pio::new(pio, Irqs);
+    let mut data = [RGB8::default(); NUM_LEDS];
+    let mut ws2812 = Ws2812::new(&mut common, sm0, dma, pin);
+    let mut ticker = Ticker::every(Duration::from_millis(100));
+    loop {
+        for j in 0..(256 * 5) {
+            for i in 0..NUM_LEDS {
+                data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
+            }
+            ws2812.write(&data).await;
+            ticker.next().await;
         }
     }
 }
